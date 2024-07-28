6bb61cb2574d3c04fd86b03fb8eb7e137f990891
DHANUSH RAJESH <148962990+Dhanu0405@users.noreply.github.com>
2024-07-28 19:56:02 +0530

    Add files via upload

--- src/data/data.js
+++ src/data/data.js
@@ -1,1 +1,27 @@
+import video from "../assets/video.png";
+import quiz from "../assets/quiz.png";
+import resume from "../assets/resume.png";
 
+export const data = [
+  {
+    id: 1,
+    name: "Resume Website",
+    image: resume,
+    github: "https://github.com/Dhanu0405/resume.git",
+    live: "https://dhanu0405.github.io/resume/",
+  },
+  {
+    id: 2,
+    name: "Interactive Quiz Application using JavaScript",
+    image: quiz,
+    github: "https://github.com/Dhanu0405/quiz-app.git",
+    live: "https://dhanu0405.github.io/quiz-app/",
+  },
+  {
+    id: 3,
+    name: "Video Meeting App using Flask",
+    image: video,
+    github: "https://github.com/Dhanu0405/flask-webapp.git",
+    live: "",
+  },
+];
